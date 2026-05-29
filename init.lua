fecf05cba67fb1dc07dae3d26a653ae41337fd62
Matt <112975732+mattpjohnston@users.noreply.github.com>
2026-05-29 23:09:12 +0100

    Add Neovim config files

---
+++ init.lua
@@ -0,0 +1,13 @@
+if vim.fn.has('nvim-0.12') ~= 1 then
+  error('This config requires Neovim 0.12 or newer')
+end
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('config.options')
+require('config.keymaps')
+require('config.autocmds')
+require('config.diagnostics')
+require('config.plugins')
+require('config.lsp')
